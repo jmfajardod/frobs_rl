42dd3ba734d3f6d9bcca5c9714d0df5eec77f3d5
Jose Fajardo <jmfajardod@unal.edu.co>
2021-10-09 09:30:29 -0500

    Update ros_node.py

--- src/frobs_rl/common/ros_node.py
+++ src/frobs_rl/common/ros_node.py
@@ -1,42 +1,39 @@
 #!/bin/python3
 
-# from rosgraph.names import namespace
 import rospy
 import rospkg
-import os
 import subprocess
-import time
+import time 
 
-
-def ROS_Node_from_pkg(pkg_name, node_name, launch_master=False, launch_master_term=True, launch_new_term=True, name=None, ns="/", output="log") -> bool:
+def ros_node_from_pkg(pkg_name, node_name, launch_master=False, launch_master_term=True, launch_new_term=True, name=None, ns="/", output="log") -> bool:
     """
     Function to launch a ROS node from a package.
 
-    :param pkg_name: Name of the package to launch the node from.
-    :type pkg_name: str
+    @param pkg_name: Name of the package to launch the node from.
+    @type pkg_name: str
 
-    :param node_name: Name of the node to launch.
-    :type node_name: str
+    @param node_name: Name of the node to launch.
+    @type node_name: str
 
-    :param launch_master: If ROSMASTER is not running launch it.
-    :type launch_master: bool
+    @param launch_master: If ROSMASTER is not running launch it.
+    @type launch_master: bool
 
-    :param launch_master_term: If launch ROSMASTER do it in an external terminal.
-    :type launch_master_term: bool
+    @param launch_master_term: If launch ROSMASTER do it in an external terminal.
+    @type launch_master_term: bool
 
-    :param launch_new_term: Launch the process in a new terminal (Xterm).
-    :type launch_new_term: bool
+    @param launch_new_term: Launch the process in a new terminal (Xterm).
+    @type launch_new_term: bool
 
-    :param name: Name to give the node to be launched.
-    :type name: str
+    @param name: Name to give the node to be launched.
+    @type name: str
 
-    :param ns: Namespace to give the node to be launched.
-    :type ns: str
+    @param ns: Namespace to give the node to be launched.
+    @type ns: str
 
-    :param output:  log, screen, or None.
-    :type output: str
+    @param output:  log, screen, or None.
+    @type output: str
 
-    :return: True if the node was launched, False otherwise.
+    @return: True if the node was launched, False otherwise.
     """
 
     rospack = rospkg.RosPack()
@@ -51,7 +48,7 @@ def ROS_Node_from_pkg(pkg_name, node_name, launch_master=False, launch_master_te
         print("Launching Master")
         try:
             rospy.get_master().getPid()
-        except:
+        except ConnectionRefusedError:
             print("Master not running")
             if launch_master_term:
                 subprocess.Popen("xterm -e 'roscore' ", shell=True)
@@ -63,7 +60,7 @@ def ROS_Node_from_pkg(pkg_name, node_name, launch_master=False, launch_master_te
 
     try:
         rospy.get_master().getPid()
-    except:
+    except ConnectionRefusedError:
         print("Master not running")
         return False
     else:
@@ -79,47 +76,47 @@ def ROS_Node_from_pkg(pkg_name, node_name, launch_master=False, launch_master_te
 
     if launch_new_term:
         term_command = "xterm -e ' " + term_command + "'"
-
+    
     subprocess.Popen(term_command, shell=True)
     time.sleep(5.0)
 
     return True
 
-def ROS_Kill_Node(node_name) -> bool:
+def ros_kill_node(node_name) -> bool:
     """
     Function to kill a ROS node.
 
-    :param node_name: Name of the node to kill.
-    :type node_name: str
+    @param node_name: Name of the node to kill.
+    @type node_name: str
 
-    :return: True if the node was killed, False otherwise.
+    @return: True if the node was killed, False otherwise.
     """
 
     term_command = "rosnode kill " + node_name
     subprocess.Popen("xterm -e ' " + term_command + "'", shell=True).wait()
     return True
 
-def ROS_Kill_All_Nodes() -> bool:
+def ros_kill_all_nodes() -> bool:
     """
     Function to kill all running ROS nodes.
 
-    :return: True if all nodes were killed, False otherwise.
+    @return: True if all nodes were killed, False otherwise.
     """
 
     term_command = "rosnode kill -a"
     subprocess.Popen("xterm -e ' " + term_command + "'", shell=True).wait()
     return True
 
-def ROS_Kill_Master() -> bool:
+def ros_kill_master() -> bool:
     """
     Function to kill the ROS master.
 
-    :return: True if the master was killed, False otherwise.
+    @return: True if the master was killed, False otherwise.
     """
 
     try:
         rospy.get_master().getPid()
-    except:
+    except ConnectionRefusedError:
         print("Master not running")
         return True
     else:
@@ -132,11 +129,11 @@ def ROS_Kill_Master() -> bool:
 
         return True
 
-def ROS_Kill_All_processes() -> bool:
+def ros_kill_all_processes() -> bool:
     """
     Function to kill all running ROS related processes.
 
-    :return: True if all processes were killed, False otherwise.
+    @return: True if all processes were killed, False otherwise.
     """
 
     term_command = "killall -9 rosout roslaunch rosmaster gzserver nodelet robot_state_publisher gzclient"
